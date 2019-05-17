b6970f381736ac8c0dedf2f54ac132f7e956fd60
tangtang233 <toby911115@163.com>
2019-05-17 15:02:38 +0800

    1.设置router读取开关

--- src/main/java/com/freelycar/saas/menu/MenuStartupRunner.java
+++ src/main/java/com/freelycar/saas/menu/MenuStartupRunner.java
@@ -5,6 +5,7 @@ import com.freelycar.saas.project.service.WxUserInfoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +18,20 @@ import org.springframework.stereotype.Component;
 public class MenuStartupRunner implements CommandLineRunner {
     private static final Logger logger = LoggerFactory.getLogger(MenuStartupRunner.class);
 
+
+    @Value("${router.init.enable}")
+    private boolean enable;
+
     @Autowired
     private WxUserInfoService wxUserInfoService;
 
     @Override
     public void run(String... args) throws Exception {
-        logger.info("startup runner");
-        WxUserInfo wxUserInfo = wxUserInfoService.findById("ea8ecbc5694d1d1d01694d2be8930000");
-        System.out.println(wxUserInfo.getTrueName());
+        //TODO 清空菜单路由表，并重新生成数据
+        if (enable) {
+            logger.info("startup runner");
+            WxUserInfo wxUserInfo = wxUserInfoService.findById("ea8ecbc5694d1d1d01694d2be8930000");
+            System.out.println(wxUserInfo.getTrueName());
+        }
     }
 }
