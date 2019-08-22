5d04f27561aaa985291e63271b124f7425454fa8
tangtang233 <toby911115@163.com>
2019-08-22 11:48:52 +0800

    :bug: Fixing a bug. - 修复合作商保存接口保存时接受到的字段都为空的问题

--- src/main/java/com/freelycar/saas/api/WebController.java
+++ src/main/java/com/freelycar/saas/api/WebController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +32,7 @@ public class WebController {
     @ApiOperation(value = "保存加盟商信息", produces = "application/json")
     @PostMapping("/saveInfo")
     @LoggerManage(description = "调用方法：保存加盟商信息")
-    public ResultJsonObject savePartnerInfo(Partner partner) {
+    public ResultJsonObject savePartnerInfo(@RequestBody Partner partner) {
         partner.setCreateTime(TimestampUtil.getCurrentTimestamp());
         return ResultJsonObject.getDefaultResult(partnerService.save(partner));
     }
