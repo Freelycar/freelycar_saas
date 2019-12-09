c71950647f5bc1c699eb124381b6e343ca53f5ec
tangtang233 <toby911115@163.com>
2019-12-09 11:31:47 +0800

    :alien: Updating code due to external API changes. - iot接口设置为过期（暂时没有任何作用）

--- src/main/java/com/freelycar/saas/project/controller/IotController.java
+++ src/main/java/com/freelycar/saas/project/controller/IotController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019-02-12
  * @email toby911115@gmail.com
  */
+@Deprecated
 @RestController
 @RequestMapping("/iot/ark")
 public class IotController {
